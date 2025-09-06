f0c16cabcd01cf13b9a86892cbfdc91965be5321
Aya Baheer git config --global user.name Aya <aya9966naj@gmail.com>
2025-09-06 22:21:54 +0300

    feat: Add reviews table with factories and seeders

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Review;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ReviewFactory extends Factory
+{
+protected $model = Review::class;
+
+
+public function definition()
+{
+return [
+'product_id' => Product::inRandomOrder()->first()->id ?? Product::factory(),
+'user_id' => User::inRandomOrder()->first()->id ?? User::factory(),
+'rating' => $this->faker->numberBetween(1, 5),
+'comment' => $this->faker->optional()->sentence(),
+];
+}
+}
