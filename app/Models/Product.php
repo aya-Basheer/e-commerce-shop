02e210ddea5686feec45f034febfdb6383df7bf3
Aya Baheer git config --global user.name Aya <aya9966naj@gmail.com>
2025-09-08 20:55:17 +0300

    feat: Add relationships between Product and Category models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,18 +9,18 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'stock',
-        'on_sale',
-        'image',
-    ];
+   
+    protected $fillable = ['name','description','price','on_sale','image','category_id','stock'];
 
     protected $casts = [
         'on_sale' => 'boolean',
         'price' => 'decimal:2',
         'stock' => 'integer',
     ];
+
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
